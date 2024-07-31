6bb753b13da9cf077449ea3db9c3485b76a13e4f
yyeonjju <llilyha123@gmail.com>
2024-07-31 21:59:33 +0900

    switch 에 대한 rxcocoa 연습

--- RXSwiftPratice/RXCocoa/SwitchViewController.swift
+++ RXSwiftPratice/RXCocoa/SwitchViewController.swift
@@ -7,7 +7,50 @@
 
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
 final class SwitchViewController : UIViewController{
+    let switchView = UISwitch()
+    let switchStatusLabel = UILabel()
     
+    
+    let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        configureUI()
+        
+        bindIsOnToLabel()
+    }
+    
+    private func bindIsOnToLabel(){
+        switchView.rx.isOn
+            .bind(with: self) { owner, isOn in
+                owner.switchStatusLabel.text = String(isOn)
+            }
+            .disposed(by: disposeBag)
+            
+    }
+    
+    
+    private func configureUI () {
+        view.addSubview(switchView)
+        view.addSubview(switchStatusLabel)
+        
+        switchView.snp.makeConstraints { make in
+            make.center.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        switchStatusLabel.snp.makeConstraints { make in
+            make.top.equalTo(switchView.snp.bottom).offset(20)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(50)
+        }
+        
+        view.backgroundColor = .white
+        switchStatusLabel.backgroundColor = .brown
+    }
 }
